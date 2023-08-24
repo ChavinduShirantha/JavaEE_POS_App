85d598a956484c1fe1720be9d5db5633ef327ec5
ChavinduShirantha <chavindushirantha@gmail.com>
2023-08-24 16:11:37 +0530

    Added doPut method to Customer servlet

--- Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
@@ -1,8 +1,6 @@
 package lk.ijse.servlet;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -130,9 +128,50 @@ public class CustomerServletAPI extends HttpServlet {
     }
 
     @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String cusID = jsonObject.getString("customerId");
+        String cusName = jsonObject.getString("customerName");
+        String cusAddress = jsonObject.getString("customerAddress");
+        String cusSalary = jsonObject.getString("customerSalary");
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+        resp.addHeader("Content-Type", "application/json");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+
+            PreparedStatement pstm3 = connection.prepareStatement("update Customer set name=?,address=?,salary=? where id=?");
+            pstm3.setObject(4, cusID);
+            pstm3.setObject(1, cusName);
+            pstm3.setObject(2, cusAddress);
+            pstm3.setObject(3, cusSalary);
+            if (pstm3.executeUpdate() > 0) {
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("state", "OK");
+                response.add("message", "Successfully Updated ! ");
+                response.add("data", "");
+                resp.setStatus(200);
+                resp.getWriter().print(response.build());
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("state", "Error");
+            objectBuilder.add("message", e.getMessage());
+            objectBuilder.add("data", "");
+            resp.setStatus(400);
+            resp.getWriter().print(objectBuilder.build());
+        }
+    }
+
+    @Override
     protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.addHeader("Access-Control-Allow-Origin", "*");
-        resp.addHeader("Access-Control-Allow-Methods", "DELETE");
+        resp.addHeader("Access-Control-Allow-Methods", "DELETE,PUT");
         resp.addHeader("Access-Control-Allow-Headers", "content-type");
     }
 }
