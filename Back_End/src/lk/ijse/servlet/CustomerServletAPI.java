3a53fd67e73d8c411344080c2b9df12ea5198c8f
ChavinduShirantha <chavindushirantha@gmail.com>
2023-08-24 15:57:09 +0530

    Added doPost method to Customer servlet

--- Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
@@ -54,4 +54,46 @@ public class CustomerServletAPI extends HttpServlet {
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cusID = req.getParameter("cusID");
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
+        String cusSalary = req.getParameter("cusSalary");
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+        resp.addHeader("Content-Type", "application/json");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+
+
+            PreparedStatement pstm = connection.prepareStatement("insert into Customer values(?,?,?,?)");
+            pstm.setObject(1, cusID);
+            pstm.setObject(2, cusName);
+            pstm.setObject(3, cusAddress);
+            pstm.setObject(4, cusSalary);
+            if (pstm.executeUpdate() > 0) {
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("state", "OK");
+                response.add("message", "Successfully Added ! ");
+                response.add("data", "");
+                resp.setStatus(200);
+                resp.getWriter().print(response.build());
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("state", "Error");
+            objectBuilder.add("message", e.getMessage());
+            objectBuilder.add("data", "");
+            resp.setStatus(400);
+            resp.getWriter().print(objectBuilder.build());
+        }
+    }
+
 }
