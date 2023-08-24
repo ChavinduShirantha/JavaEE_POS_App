9294a0eb070d351c3bfb7bcd226250446f70a396
ChavinduShirantha <chavindushirantha@gmail.com>
2023-08-24 15:51:29 +0530

    Added doGet method to Customer servlet

--- Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/servlet/CustomerServletAPI.java
@@ -1,9 +1,57 @@
 package lk.ijse.servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
 /**
  * @author : Chavindu
  * created : 8/24/2023-3:41 PM
  **/
+@WebServlet(urlPatterns = {"/pages/customer"})
+public class CustomerServletAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
 
-public class CustomerServletAPI {
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+
+            resp.addHeader("Content-Type", "application/json");
+
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                String salary = rst.getString(4);
+
+                JsonObjectBuilder customerObject = Json.createObjectBuilder();
+                customerObject.add("id", id);
+                customerObject.add("name", name);
+                customerObject.add("address", address);
+                customerObject.add("salary", salary);
+
+                allCustomers.add(customerObject.build());
+            }
+
+            resp.getWriter().print(allCustomers.build());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
