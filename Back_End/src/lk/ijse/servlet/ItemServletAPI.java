7a7dc689316a30c7ddbb7ad551c67b79ea15bfbd
ChavinduShirantha <chavindushirantha@gmail.com>
2023-08-25 15:17:06 +0530

    Added doPost method to Item servlet

--- Back_End/src/lk/ijse/servlet/ItemServletAPI.java
+++ Back_End/src/lk/ijse/servlet/ItemServletAPI.java
@@ -54,4 +54,45 @@ public class ItemServletAPI extends HttpServlet {
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("item-ID");
+        String description = req.getParameter("item-Name");
+        String price = req.getParameter("item-Price");
+        String qty = req.getParameter("item-Quantity");
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+        resp.addHeader("Content-Type", "application/json");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+
+
+            PreparedStatement pstm = connection.prepareStatement("insert into Item values(?,?,?,?)");
+            pstm.setObject(1, code);
+            pstm.setObject(2, description);
+            pstm.setObject(3, price);
+            pstm.setObject(4, qty);
+            if (pstm.executeUpdate() > 0) {
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("state", "OK");
+                response.add("message", "Successfully Added ! ");
+                response.add("data", "");
+                resp.setStatus(200);
+                resp.getWriter().print(response.build());
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("state", "Error");
+            objectBuilder.add("message", e.getMessage());
+            objectBuilder.add("data", "");
+            resp.setStatus(400);
+            resp.getWriter().print(objectBuilder.build());
+        }
+    }
 }
