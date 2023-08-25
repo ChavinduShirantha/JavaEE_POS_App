fdec87426b9886d8c5626558fbe1fa3fdc0c0e09
ChavinduShirantha <chavindushirantha@gmail.com>
2023-08-25 15:09:49 +0530

    Added doGet method to Item servlet

--- Back_End/src/lk/ijse/servlet/ItemServletAPI.java
+++ Back_End/src/lk/ijse/servlet/ItemServletAPI.java
@@ -1,9 +1,57 @@
 package lk.ijse.servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
 /**
  * @author : Chavindu
  * created : 8/24/2023-3:41 PM
  **/
+@WebServlet(urlPatterns = {"/pages/item"})
+public class ItemServletAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Item");
+            ResultSet rst = pstm.executeQuery();
 
-public class ItemServletAPI {
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+
+            resp.addHeader("Content-Type", "application/json");
+
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+            while (rst.next()) {
+                String code = rst.getString(1);
+                String description = rst.getString(2);
+                double unitPrice = rst.getDouble(3);
+                int qty = rst.getInt(4);
+
+                JsonObjectBuilder itemObject = Json.createObjectBuilder();
+                itemObject.add("code", code);
+                itemObject.add("description", description);
+                itemObject.add("unitPrice", unitPrice);
+                itemObject.add("qty", qty);
+
+                allItems.add(itemObject.build());
+            }
+
+            resp.getWriter().print(allItems.build());
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
