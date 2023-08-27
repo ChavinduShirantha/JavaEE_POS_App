43e5e22da7b7832ca6a60bf3231299a2eefab18b
ChavinduShirantha <chavindushirantha@gmail.com>
2023-08-27 09:32:42 +0530

    added orderDetails to table

--- Back_End/src/lk/ijse/servlet/PurchaseOrderServletAPI.java
+++ Back_End/src/lk/ijse/servlet/PurchaseOrderServletAPI.java
@@ -20,36 +20,36 @@ public class PurchaseOrderServletAPI extends HttpServlet {
         String option = req.getParameter("option");
         switch (option) {
             case "generateOrderID":
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
-                PreparedStatement pstm = connection.prepareStatement("SELECT oid FROM Orders ORDER BY oid DESC LIMIT 1");
-                ResultSet rst = pstm.executeQuery();
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+                    PreparedStatement pstm = connection.prepareStatement("SELECT oid FROM Orders ORDER BY oid DESC LIMIT 1");
+                    ResultSet rst = pstm.executeQuery();
 
-                resp.addHeader("Access-Control-Allow-Origin", "*");
+                    resp.addHeader("Access-Control-Allow-Origin", "*");
 
-                resp.addHeader("Content-Type", "application/json");
-
-
-                JsonArrayBuilder allOrders = Json.createArrayBuilder();
-                while (rst.next()) {
-                    String oId = rst.getString(1);
-
-                    JsonObjectBuilder orderIdObject = Json.createObjectBuilder();
-                    orderIdObject.add("oid", oId);
+                    resp.addHeader("Content-Type", "application/json");
 
 
-                    allOrders.add(orderIdObject.build());
+                    JsonArrayBuilder allOrders = Json.createArrayBuilder();
+                    while (rst.next()) {
+                        String oId = rst.getString(1);
+
+                        JsonObjectBuilder orderIdObject = Json.createObjectBuilder();
+                        orderIdObject.add("oid", oId);
+
+
+                        allOrders.add(orderIdObject.build());
+                    }
+
+                    resp.getWriter().print(allOrders.build());
+
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
                 }
-
-                resp.getWriter().print(allOrders.build());
-
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-
+                break;
             case "getOrders":
                 try {
                     Class.forName("com.mysql.jdbc.Driver");
@@ -87,6 +87,49 @@ public class PurchaseOrderServletAPI extends HttpServlet {
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
+                break;
+            case "getOrderDetails":
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee_pos", "root", "1234");
+                    PreparedStatement pstm = connection.prepareStatement("select * from OrderDetails");
+                    ResultSet rst = pstm.executeQuery();
+
+                    resp.addHeader("Access-Control-Allow-Origin", "*");
+
+                    resp.addHeader("Content-Type", "application/json");
+
+
+                    JsonArrayBuilder allOrderDetails = Json.createArrayBuilder();
+                    while (rst.next()) {
+                        String oid = rst.getString(1);
+                        String cusId = rst.getString(2);
+                        String cusName = rst.getString(3);
+                        String code = rst.getString(4);
+                        String description = rst.getString(5);
+                        String qty = rst.getString(6);
+                        String price = rst.getString(7);
+
+                        JsonObjectBuilder orderDetailObject = Json.createObjectBuilder();
+                        orderDetailObject.add("oid", oid);
+                        orderDetailObject.add("cusId", cusId);
+                        orderDetailObject.add("cusName", cusName);
+                        orderDetailObject.add("code", code);
+                        orderDetailObject.add("description", description);
+                        orderDetailObject.add("qty", qty);
+                        orderDetailObject.add("price", price);
+
+                        allOrderDetails.add(orderDetailObject.build());
+                    }
+
+                    resp.getWriter().print(allOrderDetails.build());
+
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            break;
         }
 
     }
@@ -116,16 +159,16 @@ public class PurchaseOrderServletAPI extends HttpServlet {
             connection.setAutoCommit(false);
 
             PreparedStatement pstm = connection.prepareStatement("insert into Orders values(?,?,?,?,?)");
-            pstm.setString(1,oid);
-            pstm.setString(2,cusId);
-            pstm.setString(3,date);
+            pstm.setString(1, oid);
+            pstm.setString(2, cusId);
+            pstm.setString(3, date);
             pstm.setDouble(4, Double.parseDouble(subTotal));
             pstm.setInt(5, Integer.parseInt(discount));
-            if (!(pstm.executeUpdate()>0)) {
+            if (!(pstm.executeUpdate() > 0)) {
                 connection.rollback();
                 connection.setAutoCommit(true);
                 throw new RuntimeException("Order Issue");
-            }else{
+            } else {
                 JsonArray orderDetails = jsonObject.getJsonArray("orderDetails");
 
                 for (JsonValue orderDetail : orderDetails) {
@@ -135,14 +178,14 @@ public class PurchaseOrderServletAPI extends HttpServlet {
 
 
                     PreparedStatement pstms = connection.prepareStatement("insert into OrderDetails values(?,?,?,?,?,?,?)");
-                    pstms.setObject(1,oid);
-                    pstms.setObject(2,cusId);
-                    pstms.setObject(3,name);
-                    pstms.setObject(4,code);
-                    pstms.setObject(5,description);
-                    pstms.setObject(6,qty);
-                    pstms.setObject(7,price);
-                    if (!(pstms.executeUpdate()>0)) {
+                    pstms.setObject(1, oid);
+                    pstms.setObject(2, cusId);
+                    pstms.setObject(3, name);
+                    pstms.setObject(4, code);
+                    pstms.setObject(5, description);
+                    pstms.setObject(6, qty);
+                    pstms.setObject(7, price);
+                    if (!(pstms.executeUpdate() > 0)) {
                         connection.rollback();
                         connection.setAutoCommit(true);
                         throw new RuntimeException("Order Details Issue");
@@ -153,14 +196,14 @@ public class PurchaseOrderServletAPI extends HttpServlet {
                 connection.setAutoCommit(true);
 
                 JsonObjectBuilder response = Json.createObjectBuilder();
-                response.add("state","Success");
-                response.add("message","Order Successfully Purchased..!");
-                response.add("data","");
+                response.add("state", "Success");
+                response.add("message", "Order Successfully Purchased..!");
+                response.add("data", "");
                 resp.setStatus(200);
                 resp.getWriter().print(response.build());
             }
 
-        } catch (SQLException| ClassNotFoundException | RuntimeException e) {
+        } catch (SQLException | ClassNotFoundException | RuntimeException e) {
             JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
             objectBuilder.add("state", "Error");
             objectBuilder.add("message", e.getMessage());
